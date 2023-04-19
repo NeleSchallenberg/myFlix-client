510fc260a946e299967892a6aa81681438780faa
Regina Phalangy <nschallenberg@gmx.net>
2023-04-19 11:32:31 +0200

    Clean up SignUp view

--- src/components/SignupView/SignupView.jsx
+++ src/components/SignupView/SignupView.jsx
@@ -1,9 +1,7 @@
 import { useState } from 'react';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
+import { Form, Button } from 'react-bootstrap';
 
 export const SignupView = () => {
-
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [email, setEmail] = useState('');
@@ -13,33 +11,30 @@ export const SignupView = () => {
     event.preventDefault();
 
     const data = {
-      Username: username,
-      Password: password,
-      Email: email,
-      Birthday: birthday
+      username,
+      password,
+      email,
+      birthday
     }
 
     fetch('https://female-filmmakers.herokuapp.com/users', {
       method: 'POST',
       body: JSON.stringify(data),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    }).then((response) => {
+      headers: { 'Content-Type': 'application/json' }
+    })
+    .then((response) => {
       if (response.ok) {
         alert('Signup successful!');
-        window.location.reload();
+        window.location.replace('/login');
       } else {
         alert('Signup failed.')
       }
-    });
+    })
   };
 
   return (
     <Form onSubmit={handleSubmit} className='mt-5'>
-
       <h1 className='mb-3 header'>Sign Up</h1>
-
       <Form.Group className='mb-3' controlId='formUsername'>
         <Form.Label>Username:</Form.Label>
         <Form.Control 
@@ -50,7 +45,6 @@ export const SignupView = () => {
           minLength='4'
         />
       </Form.Group>
-
       <Form.Group className='mb-3' controlId='formPassword'>
         <Form.Label>Password:</Form.Label>
         <Form.Control 
@@ -60,7 +54,6 @@ export const SignupView = () => {
           required
         />
       </Form.Group>
-
       <Form.Group className='mb-3' controlId='formEmail'>
         <Form.Label>Email:</Form.Label>
         <Form.Control
@@ -70,7 +63,6 @@ export const SignupView = () => {
           required
         />
       </Form.Group>
-
       <Form.Group className='mb-3' controlId='formBirthday'>
         <Form.Label>Birthday:</Form.Label>
         <Form.Control
@@ -80,7 +72,6 @@ export const SignupView = () => {
           required
         />
       </Form.Group>
-
       <Form.Group className='mb-3'>
         <Button 
           className='mt-2'
@@ -88,7 +79,6 @@ export const SignupView = () => {
           type='submit'
         >Submit</Button>
       </Form.Group>
-
     </Form>
   )
 }
