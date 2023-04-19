3ac6870b94cb0da81460d518feca8f5c37e52e08
Regina Phalangy <nschallenberg@gmx.net>
2023-04-19 23:00:31 +0200

    Add functions to add/remove movie

--- src/components/MovieView/MovieView.jsx
+++ src/components/MovieView/MovieView.jsx
@@ -3,11 +3,67 @@ import { Row, Col, Button, Stack } from 'react-bootstrap';
 import { useParams } from 'react-router';
 import { Link } from 'react-router-dom';
 import { MovieCard } from '../MovieCard/MovieCard';
+import { useState, useEffect } from 'react';
 
 export const MovieView = ({ movies, user, token, updateUser }) => {
   const { movieId } = useParams();
   const movie = movies.find(m => m.id === movieId);
-  const similarMovies = movies.filter(movie => movie.genre === movie.genre ? true : false)
+  const similarMovies = movies.filter(movie => movie.genre === movie.genre ? true : false);
+  const [favorite, setFavorite] = useState(user.favoriteMovies.includes(movie.id));
+
+  useEffect(() => {
+    setFavorite(user.favoriteMovies.includes(movie.id));
+  }, [movieId])
+
+  const addFavorite = () => {
+    fetch(`https://female-filmmakers.herokuapp.com/users/${user.username}/movies/${movieId}`, {
+      method: 'POST',
+      headers: { Authorization: `Bearer ${token}` }
+    })
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        alert('Movie could not be added.')
+        return false
+      }
+    })
+    .then (user => {
+      if (user) {
+        alert('Movie was added to Favorites!');
+        setFavorite(true);
+        updateUser(user);
+      }
+    })
+    .catch(e => {
+      alert(e);
+    });
+  }
+
+  const removeFavorite = () => {
+    fetch(`https://female-filmmakers.herokuapp.com/users/${user.username}/movies/${movieId}`, {
+      method: 'DELETE',
+      headers: { Authorization: `Bearer ${token}` }
+    })
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        alert('Movie could not be deleted.');
+        return false;
+      }
+    })
+    .then(user => {
+      if (user) {
+        alert('Movie was deleted from Favorites!');
+        setFavorite(false);
+        updateUser(user);
+      }
+    })
+    .catch(e => {
+      alert(e);
+    });
+  }
 
   return (
     <>
@@ -28,6 +84,10 @@ export const MovieView = ({ movies, user, token, updateUser }) => {
           <Link to={`/`}>
             <Button variant='primary' className='mb-5'>Back</Button>
           </Link>
+          {favorite ?
+            <Button variant='secondary' onClick={removeFavorite}>Remove from Favorites</Button>
+            : <Button variant='secondary' onClick={addFavorite}>Add to Favorites</Button>
+          }
         </Col>
       </Row>
       <Row>
