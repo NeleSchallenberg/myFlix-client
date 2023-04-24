0e7924ee941869b7b0e96732c3fe4f9fcef4bb90
NeleSchallenberg <nschallenberg@gmx.net>
2023-04-24 20:43:25 +0200

    Revert "3.7 client side app routing"

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,33 +1,39 @@
-import PropTypes from 'prop-types';
-import { Card, Button } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+// Import prop-types library
+import PropTypes from "prop-types";
+// Import react-bootstrap components
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 
-export const MovieCard = ({ movie }) => {
+// Exposing MovieCard component
+export const MovieCard = ({movie, onMovieClick}) => {
+  // Creating MovieCard component
   return (
-    <Card className='h-100'>
+    <Card
+      className='h-100'
+      onClick={() => {
+        onMovieClick(movie)
+      }}
+    >
       <Card.Img variant='top' src={movie.image} />
       <Card.Body>
-        <Card.Title>
-          <h4>{movie.title}</h4>
-        </Card.Title>
-        <Card.Subtitle>
-          {movie.director}
-        </Card.Subtitle>
+        <Card.Title>{movie.title}</Card.Title>
+        <Card.Subtitle>{movie.director}</Card.Subtitle>
         <Card.Text>{movie.genre}</Card.Text>
-        <Link to={`/movies/${encodeURIComponent(movie.id)}`}>
-          <Button variant='primary'>Open</Button>
-        </Link>
       </Card.Body>
     </Card>
   )
 }
 
-// PROP CONSTRAINTS
+// Define prop constraints for MovieCard
 MovieCard.propTypes = {
   movie: PropTypes.shape({
     title: PropTypes.string.isRequired,
-    genre: PropTypes.string.isRequired,
-    director: PropTypes.string.isRequired,
-    image: PropTypes.string.isRequired
-  }).isRequired
+    year: PropTypes.string,
+    length: PropTypes.string,
+    description: PropTypes.string.isRequired,
+    genre: PropTypes.string,
+    director: PropTypes.string,
+    image: PropTypes.string
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired
 };
